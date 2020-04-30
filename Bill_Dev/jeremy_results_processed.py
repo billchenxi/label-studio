d2de38ba31d5742c3a5c3df79f3b5aace49bdb1a
Bill Chen <billchenxi@gmail.com>
2020-04-30 07:54:14 -0700

    Finished the JO's results

--- Bill_Dev/jeremy_results_processed.py
+++ Bill_Dev/jeremy_results_processed.py
@@ -1,18 +1,35 @@
 import pandas as pd
-from pprint import pprint 
+from pprint import pprint
+import json
 
 JR_csv = pd.read_csv("Finished_labels/Jeremy_5_abs_results.csv")
 # pprint(JR_csv)
 
-df = pd.DataFrame()
+output = []
 for index, row in JR_csv.iterrows():
-    temp = row['Relationship']
-    pprint(type(temp))
-    temp = pd.DataFrame(temp)
+    temp = json.loads(row['Relationship'])
+    temp_dict = {}
+    temp_dict["completions"] = [{"result": []}]
+    for elem in temp:
+        temp_input = {
+                "from_name": "Relationship",
+                "source": "$text",
+                "to_name": "text",
+                "type": "labels",
+                "value": elem}
+
+        temp_dict["completions"][0]["result"].append(temp_input)
+
+    temp_dict["data"] = {"text": row['text']}
+
+    output.append(temp_dict)
+
+    # temp = pd.DataFrame(temp)
     # id = row['text'].split("\n")[0]
     # for i in range(len(temp)):
     #     print(temp[i])
     #     # temp[i]['ID'] = id
     # df = pd.concat([df, temp])
-    
-pprint(df)
+with open("../JO_results_processed.json", 'w') as jf:
+    json.dump(output, jf)
+pprint(output)
